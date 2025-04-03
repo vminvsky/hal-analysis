11765d3497c5ebe3f8492617f0d627bd72b80618
nnadgi01 <nityanad8@gmail.com>
2025-04-03 10:33:58 -0400

    adding visualizations

--- accuracy.py
+++ accuracy.py
@@ -8,7 +8,7 @@ def main():
     # Load data for all tasks you want to analyze
     tasks = ['taubench_retail', 'usaco', 'test','taubench', 'swebench', 'react', 'planexec', 'ipfuncall', 'inspect', 'gaia', 'fullcode', 'cybench', 'agentharm_', 'agentharm_benign']  # Add your task names here
     # tasks = ['gaia', 'cybench', 'taubench', 'taubench_retail' , 'agentharm_']
-    cols = ['model_name_short', 'accuracy', 'benchmark_name', 'agent_name']
+    cols = ['model_name_short', 'accuracy', 'benchmark_name', 'agent_name', 'agent_name_short']
     all_data = []
     all_data = pd.DataFrame()
     for task in tasks:
@@ -24,9 +24,9 @@ def main():
     df = all_data
     print(df.keys())
     model_accuracy = df.groupby(['model_name_short', 'benchmark_name'])[['accuracy']].mean()
-    benchmark_accuracy = df.groupby(['agent_name', 'benchmark_name'])[['accuracy']].mean()
-    # model_accuracy.to_csv('model_accuracy.csv')
-    # benchmark_accuracy.to_csv('benchmark_accuracy.csv')
+    benchmark_accuracy = df.groupby(['agent_name_short', 'benchmark_name'])[['accuracy']].mean()
+    model_accuracy.to_csv('model_accuracy.csv')
+    benchmark_accuracy.to_csv('benchmark_accuracy.csv')
 
     # mean accuracy of models across benchmarks
     model_mean_accuracy = model_accuracy.groupby('model_name_short')['accuracy'].mean().reset_index()
@@ -34,7 +34,7 @@ def main():
     model_mean_accuracy.to_csv('data/model_mean_accuracy.csv')
 
     # mean accuracy of agents across benchmarks
-    agent_mean_accuracy = benchmark_accuracy.groupby('agent_name')['accuracy'].mean().reset_index()
+    agent_mean_accuracy = benchmark_accuracy.groupby('agent_name_short')['accuracy'].mean().reset_index()
     agent_mean_accuracy = agent_mean_accuracy.rename(columns={'accuracy':'mean_accuracy'})
     agent_mean_accuracy.to_csv('data/agent_mean_accuracy.csv')
 
--- token_usage.py
+++ token_usage.py
@@ -8,7 +8,7 @@ def main():
     # Load data for all tasks you want to analyze
     # tasks = ['taubench_retail', 'usaco', 'test','taubench', 'swebench', 'react', 'planexec', 'ipfuncall', 'inspect', 'gaia', 'fullcode', 'cybench', 'agentharm_', 'agentharm_benign']  # Add your task names here
     tasks = ['gaia', 'cybench', 'taubench', 'taubench_retail' , 'agentharm_']
-    cols = ['model_name_short', 'total_cost', 'benchmark_name', 'agent_name']
+    cols = ['model_name_short', 'total_cost', 'benchmark_name', 'agent_name', 'agent_name_short']
     all_data = []
     all_data = pd.DataFrame()
     for task in tasks:
@@ -24,18 +24,18 @@ def main():
     df = all_data
     print(df.keys())
     model_total_usage = df.groupby(['model_name_short', 'benchmark_name'])[['total_cost']].mean()
-    benchmark_total_usage = df.groupby(['agent_name', 'benchmark_name'])[['total_cost']].mean()
-    # model_total_usage.to_csv('model_total_usage.csv')
-    # benchmark_total_usage.to_csv('benchmark_total_usage.csv')
+    benchmark_total_usage = df.groupby(['agent_name_short', 'benchmark_name'])[['total_cost']].mean()
+    model_total_usage.to_csv('model_total_usage.csv')
+    benchmark_total_usage.to_csv('benchmark_total_usage.csv')
 
     # mean cost of models across benchmarks
     model_mean_cost = model_total_usage.groupby('model_name_short')['total_cost'].mean().reset_index()
-    model_mean_cost = model_mean_cost.rename(columns={'cost':'mean_cost'})
+    model_mean_cost = model_mean_cost.rename(columns={'total_cost':'mean_cost'})
     model_mean_cost.to_csv('data/model_mean_cost.csv')
 
-    # todo: mean cost of agents across benchmarks
-    agent_mean_cost =  benchmark_total_usage.groupby('agent_name')['total_cost'].mean().reset_index()
-    agent_mean_cost = agent_mean_cost.rename(columns={'cost':'mean_cost'})
+    # mean cost of agents across benchmarks
+    agent_mean_cost =  benchmark_total_usage.groupby('agent_name_short')['total_cost'].mean().reset_index()
+    agent_mean_cost = agent_mean_cost.rename(columns={'total_cost':'mean_cost'})
     agent_mean_cost.to_csv('data/agent_mean_cost.csv')
 
 if __name__ == "__main__":
